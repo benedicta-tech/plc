feaf79a5cbfca46763cacd15f0dca899799d613e
Duke <emersonalmeidax@gmail.com>
2025-11-09 02:23:28 -0300

    fix: Corrigindo integração com banco de dados

--- lib/core/config/env.dart
+++ lib/core/config/env.dart
@@ -4,6 +4,6 @@ part 'env.g.dart';
 
 @Envied(path: '.env')
 abstract class Env {
-  @EnviedField(varName: 'GOOGLE_SERVICE_ACCOUNT')
+  @EnviedField(varName: 'GOOGLE_SERVICE_ACCOUNT', rawString: true)
   static final String googleServiceAccount = _Env.googleServiceAccount;
 }
